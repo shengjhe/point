32596f1e73ff0fd310640fcc009f75897c25dfd6
shengjhe <shengjhe@gmail.com>
2013-08-13 16:53:59 +0800

    Sensor 編號修正

--- point_ok.ino
+++ point_ok.ino
@@ -1,23 +1,45 @@
+/******************************************************
+**              主機參數設定                        **
+******************************************************/
 int signal=0;
 int lastStatus=0;
 long  updatatime=0;
 int startonce=3;
+String sensorid="A0001@";
+
+/******************************************************
+**                 初始化設定                        **
+******************************************************/
+
 void setup()
 {
   pinMode(2,OUTPUT);
   pinMode(3,INPUT);
   Serial.begin(9600);
-  Serial.write(startonce);
+  int cnt=0;
+  if(cnt==0)
+  {
+    cnt++;
+    Serial.print(sensorid+startonce);
+    
+  }
   delay(3000);
 }
-void loop(){
+
+/******************************************************
+**           Arduino 循環函數                        **
+******************************************************/
+
+void loop()
+{
    digitalWrite(2,HIGH);
    signal=digitalRead(3);
    if( signal != lastStatus ||millis() > updatatime)
   {
-        Serial.write(signal);
+        Serial.print(sensorid+signal);
         updatatime=millis()+60000;
-        delay(500);
+        delay(1000);
   }
   lastStatus=signal;
 }
+
