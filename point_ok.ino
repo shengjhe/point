d6cf9c3a9c74cbde6abcd65b3d06c2162b395356
shengjhe <shengjhe@gmail.com>
2013-08-16 17:03:59 +0800

    增加電壓量測函數

--- point_ok.ino
+++ point_ok.ino
@@ -5,7 +5,19 @@ int signal=0;
 int lastStatus=0;
 long  updatatime=0;
 int startonce=3;
-String sensorid="A0001@";
+String sensorid="A01";
+String result ="";
+
+
+/******************************************************
+**                電壓量測參數設定                   **
+******************************************************/
+int sensorPin = A0;    // 量測電壓腳位
+int sensorValue = 0;  // 讀取電壓原始資料
+float Voltage ;   //儲存運算後電壓值
+
+
+
 
 /******************************************************
 **                 初始化設定                        **
@@ -20,25 +32,48 @@ void setup()
   if(cnt==0)
   {
     cnt++;
-    Serial.print(sensorid+startonce);
+    getVoltage();
+    result=sensorid+startonce;
+    result+= String(Voltage, 2);
+    Serial.print(result);
     
   }
   delay(3000);
 }
 
 /******************************************************
+**           Arduino 量測電壓函數                    **
+******************************************************/
+void getVoltage()
+{
+
+   Voltage=0;
+   sensorValue = analogRead(sensorPin); 
+   Voltage = (sensorValue*(5.0 / 1023.0))*2; 
+}
+
+
+
+/******************************************************
 **           Arduino 循環函數                        **
 ******************************************************/
-
 void loop()
 {
+   result="";
    digitalWrite(2,HIGH);
+
    signal=digitalRead(3);
+   delay(1000);
+
+   
    if( signal != lastStatus ||millis() > updatatime)
   {
-        Serial.print(sensorid+signal);
+        getVoltage();
+        result=sensorid+signal;
+        result+= String(Voltage, 2);
+        Serial.print(result);
         updatatime=millis()+60000;
-        delay(1000);
+        delay(1500);
   }
   lastStatus=signal;
 }
